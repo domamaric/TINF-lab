1f8a6880a4bfb41352b648441df49b08ae735a17
domamaric <domamaric20@gmail.com>
2021-01-08 13:34:57 +0100

    Code output finished

--- Code/HuffmanCoding.py
+++ Code/HuffmanCoding.py
@@ -1,28 +1,28 @@
-global probabilities
-probabilities = []
+class HuffmanTree:
+    def __init__(self, left=None, right=None):
+        self.left = left
+        self.right = right
+
+    def children(self):
+        return (self.left, self.right)
+
+    def nodes(self):
+        return (self.left, self.right)
+
+    def __str__(self):
+        return '%s_%s' % (self.left, self.right)
 
 
-class HuffmanCodeNode:
-    def __init__(self, p):
-        self.p = p
-        self.left = None
-        self.right = None
+def HuffmanCode(node, left=True, digit=''):
+    if type(node) is str:
+        return {node: digit}
 
-    def insert(self, data):
-    # Compare the new value with the parent node
-        if self.data:
-            if data < self.data:
-                if self.left is None:
-                    self.left = Node(data)
-                else:
-                    self.left.insert(data)
-            elif data > self.data:
-                if self.right is None:
-                    self.right = Node(data)
-                else:
-                    self.right.insert(data)
-        else:
-            self.data = data
+    (l, r) = node.children()
+    huffman_code = {}
+    huffman_code.update(HuffmanCode(l, True, digit + '0'))
+    huffman_code.update(HuffmanCode(r, False, digit + '1'))
+
+    return huffman_code
 
 
 def main():
@@ -37,15 +37,25 @@ def main():
         else:
             frequencies[symbol] = 1
 
-    frequencies = sorted(frequencies.items(), key=lambda x: x[1], reverse=True)
+    frequencies = sorted(frequencies.items(),
+                         key=lambda x: x[1], reverse=True)
 
-    probabilities = [float("{:.3f}".format(frequency[1]/length))
-                     for frequency in frequencies]
-    probabilities = sorted(probabilities, reverse=True)
+    nodes = frequencies
+    while len(nodes) > 1:
+        (key1, val1) = nodes[-1]
+        (key2, val2) = nodes[-2]
+        nodes = nodes[:-2]
+        node = HuffmanTree(key1, key2)
+        nodes.append((node, val1 + val2))
 
-    huffmanCode = HuffmanCodeNode(probabilities)
+        nodes = sorted(nodes, key=lambda x: x[1], reverse=True)
 
-    print(probabilities)
+    code = HuffmanCode(nodes[0][0])
+
+    print(' Znak | Kod     ')
+    print('----------------')
+    for (char, frequency) in frequencies:
+        print(' %-4r |%6s' % (char, code[char]))
 
 
 main()
